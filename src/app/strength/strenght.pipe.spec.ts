67b5754b35858bb1b7e682d831b6d28214699888
jsizemo <jason.sizemore@e-farmcredit.com>
2023-03-14 16:23:34 -0400

    Pipe strength test

---
+++ src/app/strength/strenght.pipe.spec.ts
@@ -0,0 +1,23 @@
+import { StrengthPipe } from './strength.pipe';
+describe('StrengthPipe', () => {
+    it('should display weak if strength is 5', () => {
+        let pipe = new StrengthPipe() 
+        let val = pipe.transform(5)
+
+        expect(val).toEqual('5 (weak)');
+    })
+    it('should display strong if strength is 10', () => {
+        let pipe = new StrengthPipe();
+        let val = pipe.transform(10);
+
+        expect(val).toEqual("10 (strong)");
+
+    })
+
+     it("should display unbelievable if strength is 22", () => {
+       let pipe = new StrengthPipe();
+       let val = pipe.transform(22);
+
+       expect(val).toEqual("22 (unbelievable)");
+     });
+})
