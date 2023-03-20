c7e0623a1d34ce4367fd768d212b17447e81ad8a
jsizemo <jason.sizemore@e-farmcredit.com>
2023-03-20 16:33:00 -0400

    Added subscribe test

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -2,6 +2,7 @@ import { Hero } from './../hero';
 import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes.component';
 import {of} from 'rxjs';
+import { inject, waitForAsync } from '@angular/core/testing';
 
 describe ('HeroesComponent', () => {
     let component: HeroesComponent
@@ -22,7 +23,7 @@ describe ('HeroesComponent', () => {
 
     describe('delete', () => {
        it('should remove the indicated hero from the heros list', () => {
-        mockHeroService.deleteHero.and.returnValue(of(true))
+         mockHeroService.deleteHero.and.returnValue(of(true));
         component.heroes = HEROES;
 
         component.delete(HEROES[1]);
@@ -41,15 +42,21 @@ describe ('HeroesComponent', () => {
 
          expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
        });
-       it('should be subscribed to the observable', () => {
-         mockHeroService.deleteHero.and.returnValue(of(true));
+
+       it('should subscribe to deleteHero method', () => {
+        
+        //Arrange
+        mockHeroService.deleteHero.and.returnValue(of(true));
+        const spyObj = spyOn(mockHeroService.deleteHero(), 'subscribe');
          component.heroes = HEROES;
-
+         
+         //Act
          component.delete(HEROES[2]);
+        
+         //Assert
+         expect(spyObj).toHaveBeenCalled()
 
-         expect(mockHeroService).toBeDefined();
-
-         //TODO:  check that we are subscribing to the result of the deleteHero call
-       });
+       })
+    
     })
 })
